8b81cb564d6ba1cead0a8a28571ac2215ece4986
Vivek Panchal <vivek.panchal@esparkbizmail.com>
2024-05-29 19:49:57 +0530

    Setup the basic folder structure

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const { config } = require("dotenv");
+config({ path: `.env` });
+const router = require("./routes/index-route");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use("/", router);
+
+const PORT = process.env.API_PORT;
+app.listen(PORT, () => {
+  console.log("=================================");
+  console.log(`🚀 App listening on the port ${PORT}`);
+  console.log("=================================");
+});
