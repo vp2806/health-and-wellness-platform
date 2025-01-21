86f3fcd4d15513ffb66aaa1dabf4754492207da8
Vivek Panchal <vivekpanchalp03@gmail.com>
2025-01-22 01:08:17 +0530

    refactor: Sequelize connection config

--- config/config.js
+++ config/config.js
@@ -5,6 +5,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
-  host: "127.0.0.1",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: "mysql",
 };
