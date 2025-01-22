65e5db6f2619cd03a5a046e55a6e68d0b8f7bae5
Vivek Panchal <vivekpanchalp03@gmail.com>
2025-01-23 00:17:00 +0530

    fix: not access public assets issue

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require("./jobs/medication-notification-job");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(cookieParser());
-app.use("/public", express.static("public"));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
